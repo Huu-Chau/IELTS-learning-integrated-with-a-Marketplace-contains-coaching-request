7e8dd04ed3d19fee5feb932024c29e37a3649059
Viet Phung <viet.nexlab@carehealth.io>
2026-05-03 20:26:18 +0700

    handle writing session notifications

--- server/src/consumers/index.ts
+++ server/src/consumers/index.ts
@@ -2,3 +2,8 @@
 export interface IConsumer {
     consume(): Promise<void>;
 }
+
+export * from './notification-on-attempt-created';
+export * from './notification-on-marketplace-request-status-updated';
+export * from './notification-on-writing-session-status-updated';
+export * from './notification-on-marketplace-request-created';
